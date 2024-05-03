c61190565f73ed1e4b783345091121285da7ac37
Brice <99179318+BriceLucifer@users.noreply.github.com>
2024-05-03 12:05:22 +0800

    Update rust.sh

--- rust.sh
+++ rust.sh
@@ -9,19 +9,6 @@ curl https://sh.rustup.rs -sSf | sh
 
 source $HOME/.cargo/env
 
-# 配置crate镜像 自动化了 
-echo "rust crate 镜像自动配置 ustc"
-# rust安装加速
-
-export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static
-export RUSTUP_UPDATE_ROOT=https://mirrors.ustc.edu.cn/rust-static/rustup
-
-curl https://sh.rustup.rs -sSf | sh
-
-# 安装完成
-
-source $HOME/.cargo/env
-
 echo "开始配置镜像源 提高crate拉取速度 使用utsc"
 
 mkdir -vp ${CARGO_HOME:-$HOME/.cargo}
