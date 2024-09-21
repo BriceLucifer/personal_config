3451284798a5e067982cda47c0e136b739a0d320
Brice <99179318+BriceLucifer@users.noreply.github.com>
2024-09-21 13:00:33 +0800

    Update install-ferdora.sh

--- install-ferdora.sh
+++ install-ferdora.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "Setting up for new Fedora installation";
+echo "Setting up for new Fedora installation"
 
 # 更新系统
 sudo dnf update
 
 # 安装基本工具和编辑器
 sudo dnf install -y @development-tools vim git make cmake
-sudo dnf install -y neovim
+sudo dnf install -y neovim 
 
 echo "nvim vim git make cmake is Complete"
